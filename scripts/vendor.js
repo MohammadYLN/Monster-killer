17c2849e45381b18322671860c8f25600ce647f0
Mohammad Yalaan <mohamadyalaan@gmail.com>
2020-03-15 17:19:19 +0330

    add some function for bars and players life

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -6,3 +6,40 @@ const attackBtn = document.getElementById('attack-btn');
 const strongAttackBtn = document.getElementById('stront-attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
+
+function adjustHealthBar(maxlife){
+    monsterHealthBar.max = maxlife;
+    monsterHealthBar.value = maxlife;
+    playerHealthBar.max = maxlife;
+    playerHealthBar.value = maxlife;
+}
+
+function dealMonsterDamage(damage){
+    const dealtDamage = Math.random() * damage;
+    monsterHealthBar = +monsterHealthBar - dealtDamage;
+    return dealtDamage;
+}
+
+function dealtPlayerDamage(damage){
+    const dealtDamage = Math.random() * damage;
+    playerHealthBar = +playerHealthBar - dealtDamage;
+    return dealtDamage;
+}
+
+function increasePlayerHealth(healValue){
+    playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value){
+    monsterHealthBar.value = value;
+    playerHealthBar.value = value;
+}
+
+function removeBonusLife() {
+    bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+  }
+  
+  function setPlayerHealth(health) {
+    playerHealthBar.value = health;
+  }
+  
