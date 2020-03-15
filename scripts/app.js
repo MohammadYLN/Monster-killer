0d21619c4e86d85b53d1be3347ee0a2b03a1842f
Mohammad Yalaan <mohamadyalaan@gmail.com>
2020-03-15 19:03:50 +0330

    adding function end game

--- scripts/app.js
+++ scripts/app.js
@@ -12,4 +12,35 @@ if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
+let hasBonusLife = true;
 
+adjustHealthBars(chosenMaxLife);
+
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
+    const playerDamage = dealtPlayerDamage(ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if(currentPlayerHealth <=0 && hasBonusLife){
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('You would be dead but the bonus life saved you!');
+    }
+    if(currentPlayerHealth > 0 && currentMonsterHealth <=0 ){
+        alert("You Won!");
+    }else if(currentMonsterHealth > 0 && currentPlayerHealth <=0){
+        alert('Sorry,You are dead!');
+    }else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+        alert('You have draw!');
+    }
+    if(currentMonsterHealth <= 0 || currentPlayerHealth <= 0){
+        Game();
+    }
+}
